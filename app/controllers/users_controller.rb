9f826cfd6e7c5ae1ef7f96bcb15bba15a9aef553
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-14 21:09:34 -0400

    Adds backend user authentication and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,10 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    # p current_user
+    user = User.find_by(id: params[:id])
+    render json: user.as_json
+  end
 end
